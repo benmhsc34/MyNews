963c40de0ffc997b556ad7104cad9c1b6fde8aaf
Benjamin Corben <corbenbenjamin@gmail.com>
2019-01-24 15:23:49 +0100

    Nav drawer but onclick events not handled

--- app/src/main/java/com/example/benja/myapplication/MainActivity.java
+++ app/src/main/java/com/example/benja/myapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.benja.myapplication;
 
+import android.app.ActionBar;
 import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -13,10 +20,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.benja.myapplication.Tabs.CustomStoriesTab;
 import com.example.benja.myapplication.Tabs.PopularStoriesTab;
@@ -26,10 +35,14 @@ import com.example.benja.myapplication.Toolbar.HelpActivity;
 import com.example.benja.myapplication.Toolbar.NotificationActivity;
 import com.example.benja.myapplication.Toolbar.SearchActivity;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
 import static android.app.Notification.CATEGORY_MESSAGE;
 import static com.example.benja.myapplication.MyNews.CHANNEL_1_ID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -45,15 +58,19 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
-
-
+        drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle mDrawerToggle;
 
         ImageView searchIcon = findViewById(R.id.searchIcon);
         searchIcon.setOnClickListener(new View.OnClickListener() {
@@ -66,12 +83,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_reorder_black_24dp);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -87,6 +101,14 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Close the menu so open and if the touch return is pushed
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 
 
@@ -122,8 +144,40 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         }
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
 
         return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+
+
+            case R.id.nav_top:
+                Intent myIntent = new Intent(this, TopStoriesTab.class);
+                startActivity(myIntent);
+                break;
+            case R.id.nav_pop:
+                PopularStoriesTab popTab = new PopularStoriesTab();
+                this.getSupportFragmentManager().beginTransaction().replace(R.id.activity_welcome_coordinator_layout, popTab, "Find top fragment").commit();
+                break;
+            case R.id.nav_cus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.tabs, new CustomStoriesTab()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "You share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_contactUs:
+                Toast.makeText(this, "You want to contact us x)", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
@@ -137,12 +191,16 @@ public class MainActivity extends AppCompatActivity {
             super(fm);
         }
 
+
+
         @Override
         public Fragment getItem(int position) {
+
             switch (position) {
                 case 0:
                     TopStoriesTab tab1 = new TopStoriesTab();
                     return tab1;
+
                 case 1:
                     PopularStoriesTab tab2 = new PopularStoriesTab();
                     return tab2;
